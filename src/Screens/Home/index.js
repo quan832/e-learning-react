be89c549db0e6491f4df4028985fd0e6170efa32
quantran2381@gmail.com <quantran2381@gmail.com>
2021-02-11 20:26:38 +0700

    fist commit

---
+++ src/Screens/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4">Danh Sach Khoa Hoc</h1>
+            </div>
+        )
+    }
+}
